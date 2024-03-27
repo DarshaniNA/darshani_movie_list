0d3d1d2f32c74365e1a0d08008aff1620de1fb2a
DarshaniNA <nisansalabeywickrama@gmail.com>
2024-03-27 11:13:48 +0000

    added navigation component

---
+++ app/src/main/java/com/darshani/movielist/Navigation.kt
@@ -0,0 +1,74 @@
+package com.darshani.movielist
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.darshani.movielist.ui.login.LoginScreen
+import com.darshani.movielist.ui.moviesList.MovieListScreen
+import com.darshani.movielist.ui.signup.SignUpScreen
+
+sealed class Route {
+    data class LoginScreen(val name: String = "Login") : Route()
+    data class SignUpScreen(val name: String = "SignUp") : Route()
+    data class MovieListScreen(val name: String = "Home") : Route()
+}
+
+@Composable
+fun Navigation(navHostController: NavHostController) {
+    NavHost(
+        navController = navHostController,
+        startDestination = "login_flow",
+    ) {
+        navigation(startDestination = Route.LoginScreen().name, route = "login_flow") {
+            composable(route = Route.LoginScreen().name) {
+                LoginScreen(
+                    onLoginClick = {
+                        navHostController.navigate(
+                            Route.MovieListScreen().name
+                        ) {
+                            popUpTo(route = "login_flow")
+                        }
+                    },
+                    onSignUpClick = {
+                        navHostController.navigateToSingleTop(
+                            Route.SignUpScreen().name
+                        )
+                    }
+                )
+            }
+            composable(route = Route.SignUpScreen().name) {
+                SignUpScreen(
+                    onSignUpClick = {
+                        navHostController.navigate(
+                            Route.MovieListScreen().name
+                        ){
+                            popUpTo("login_flow")
+                        }
+                    },
+                    onLoginClick = {
+                        navHostController.navigateToSingleTop(
+                            Route.LoginScreen().name
+                        )
+                    }
+                )
+            }
+        }
+        composable(route = Route.MovieListScreen().name) {
+            MovieListScreen()
+        }
+    }
+}
+
+fun NavController.navigateToSingleTop(route: String) {
+    navigate(route) {
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
+}
